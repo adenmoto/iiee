22d0b65a77c4df6c20f34f87cddc059ab0c67f31
nephila <nephila@nephila-H81M-DS2>
2017-12-13 14:34:26 +0800

    lalalalala

--- resources/views/IEEE/about/about.blade.php
+++ resources/views/IEEE/about/about.blade.php
@@ -1,84 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>About Us</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <!-- Navigation -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="about.html">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="services.html">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Portfolio
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
-                <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Blog
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
-                <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-                <a class="dropdown-item" href="blog-post.html">Blog Post</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Other Pages
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                <a class="dropdown-item" href="full-width.html">Full Width Page</a>
-                <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
-                <a class="dropdown-item" href="faq.html">FAQ</a>
-                <a class="dropdown-item" href="404.html">404</a>
-                <a class="dropdown-item" href="pricing.html">Pricing Table</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+@extends('layouts.app')
+@section('content')   
     <!-- Page Content -->
-    <div class="container">
+    <div class="container   ">
 
       <!-- Page Heading/Breadcrumbs -->
       <h1 class="mt-4 mb-3">About Us
@@ -94,73 +17,15 @@
       <!-- Intro Content -->
       <div class="row">
         <div class="col-lg-6">
-          <img class="img-fluid rounded mb-4" src="images/aboutx/aboutlogo.png"alt="">
+          <img class="img-fluid rounded mb-4 animated fadeIn" src="images/aboutx/aboutlogo.png"alt="">
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-6 animated fadeIn">
           <h2>About IEEE</h2>
           <p>The Institute of Integrated Electrical Engineers of the Philippines, Inc. (IIEE) is the organization of electrical practitioners and the only accredited professional organization (APO) of electrical practitioners by the Professional Regulation Commission (PRC) with PRC Cert. No. I-APO-016. Founded in 1975, the organization, which started with 500 members, has now 47,000 members and more are joining.</p>
           <p>The new EE Law (RA 7920) requires Electrical Practitioners (Professional Electrical Engineers, Registered Electrical Engineers and Registered Master Electricians) to be a member of IIEE or other PRC accredited organization of electrical practitioners. IIEE being the only APO of electrical practitioners expects to more than double its membership by year 2008.</p>
           <p>IIEE is committed to (1) upgrade the professional credentials of its members through the Continuing Professional Development (CPD) programs; (2) enhance their social standing through the promotion of high ideals of integrity and ethics; (3) facilitate advancement of technology transfer through carefully conceived educational and training programs; and (4) provide opportunity for regular interaction among electrical practitioners.</p>
         </div>
       </div>
-      <!-- /.row -->
-
-      <!-- Team Members -->
-      <h2>Our Goals</h2>
-
-      <div class="row">
-        <div class="col-lg-4 mb-4">
-          <div class="card h-100 text-center">
-            <img class="card-img-top" src="images/aboutx/visionmission.png" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Vision/Mission</h4>
-              <p class="card-text">To enhance the competencies of electrical practitioners to make positive contribution towards new trends and technologies.</p>
-            </div>
-            <div class="card-footer">
-                <a href="vision">Vision/Mission</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-          <div class="card h-100 text-center">
-            <img class="card-img-top" src="images/aboutx/codeofethics.png" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Code of Ethics</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#">name@example.com</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-          <div class="card h-100 text-center">
-            <img class="card-img-top" src="images/aboutx/objectives.png" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Objectives</h4>
-              <p class="card-text">To enroll the membership and unite all registered electrical engineers and master electricians.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#">name@example.com</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /.container -->
-
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
-</html>
+@extends('layouts.appabout')
+@section('content') 
+@endsection
