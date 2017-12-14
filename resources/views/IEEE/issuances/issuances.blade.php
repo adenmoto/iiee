88486b3b56d1bcb53f1d67861be49ae46196d191
nephila <nephila@nephila-H81M-DS2>
2017-12-14 10:22:38 +0800

    lalalalala

--- resources/views/IEEE/issuances/issuances.blade.php
+++ resources/views/IEEE/issuances/issuances.blade.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Modern Business - Start Bootstrap Template</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
-
-  </head>
 @extends('layouts.app')
 @section('content')
-
     <!-- Page Content -->
     <div class="container">
 
@@ -76,7 +56,6 @@
         </div>
       </div>
       <!-- /.row -->
-
     </div>
     <!-- /.container -->
 @endsection
